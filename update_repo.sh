a2543e2c5519269c6c539116410e311feb5a4a4f
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2019-08-22 14:55:49 +0200

    Test

--- update_repo.sh
+++ update_repo.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# You need to adapt these variables
-REPO_ROOT="./"
 
 # You should first put desired deb files in the deb folder
 
@@ -12,9 +10,9 @@ rm ${REPO_ROOT}Packages.bz2
 rm ${REPO_ROOT}Packages.xz
 
 # Generate Packages file
-dpkg-scanpackages -m ${REPO_ROOT}deb /dev/null > ${REPO_ROOT}Packages
+dpkg-scanpackages -m . /dev/null > Packages
 
 # Generate Packages.* comppressed files
-gzip -c9 ${REPO_ROOT}Packages > ${REPO_ROOT}Packages.gz
-bzip2 -c9 ${REPO_ROOT}Packages > ${REPO_ROOT}Packages.bz2
-xz -c9 ${REPO_ROOT}Packages > ${REPO_ROOT}Packages.xz
+gzip -c9 Packages > Packages.gz
+bzip2 -c9 Packages > Packages.bz2
+xz -c9 Packages > Packages.xz
