1a52976c2bf7e4101f337a26e3eaf9754a930da0
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2019-08-22 14:24:48 +0200

    first commit

---
+++ tools/update_repo.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# You need to adapt these variables
+REPO_ROOT="../"
+
+# You should first put desired deb files in the deb folder
+
+# Clean old files
+rm ${REPO_ROOT}Packages
+rm ${REPO_ROOT}Packages.gz
+rm ${REPO_ROOT}Packages.bz2
+rm ${REPO_ROOT}Packages.xz
+
+# Generate Packages file
+dpkg-scanpackages -m ${REPO_ROOT}deb /dev/null > ${REPO_ROOT}Packages
+
+# Generate Packages.* comppressed files
+gzip -c9 ${REPO_ROOT}Packages > ${REPO_ROOT}Packages.gz
+bzip2 -c9 ${REPO_ROOT}Packages > ${REPO_ROOT}Packages.bz2
+xz -c9 ${REPO_ROOT}Packages > ${REPO_ROOT}Packages.xz
